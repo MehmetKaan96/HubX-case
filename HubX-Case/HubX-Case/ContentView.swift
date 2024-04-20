753da93f8668b0ffd2307cf65c5e59ebba509a8b
Mehmet Kaan <mehmetkaan1996@gmail.com>
2024-04-20 17:03:04 +0300

    initial commit

---
+++ HubX-Case/HubX-Case/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HubX-Case
+//
+//  Created by Mehmet Kaan on 20.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HubX-Case/HubX-Case/HubX_CaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HubX_CaseApp.swift
+//  HubX-Case
+//
+//  Created by Mehmet Kaan on 20.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct HubX_CaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
